f8f5c78bf1ef1889f1c2b7b4232a4a88841ccfab
nairaquel <raquelnaira2020@gmail.com>
2025-02-17 19:48:07 +0000

    menssagem de salvamento

---
+++ 2.estrutura de repeticao/1.estrutura _simples.py
@@ -0,0 +1,14 @@
+import os 
+os.system ("clear")
+
+idade = 10
+
+# SE IDADE < 18 entao
+#       escreval ("acesso negado")
+
+if idade < 18:
+
+    print(" Acesso negado. ")
+    
+# fimse
+print("==FIM==")
